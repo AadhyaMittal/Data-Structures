35a6338abd60839cbb32e6912ad68cd147418a92
Aadhya Mittal <ammittalaadhya@gmail.com>
2017-01-26 07:45:56 -0800

    Add files via upload

---
+++ linkedlist_deletion_at_begin.c
@@ -0,0 +1,46 @@
+//Deletion at Beginning
+
+#include<stdio.h>
+#include<conio.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+void insert_beg(struct node **head, int data)
+{
+    struct node *new_node = (struct node*)malloc(sizeof(struct node));
+    new_node -> data = data;
+    new_node -> next = *head;
+    *head = new_node;
+}
+
+void print(struct node *head)
+{
+    while(head != NULL)
+    {
+        printf("%d ", head -> data);
+        head = head -> next;
+    }
+}
+
+void delete_begin(struct node **head)
+{
+    struct node *temp = *head;
+    *head = temp -> next;
+    free(temp);
+}
+int main()
+{
+        struct node *head = NULL;
+        insert_beg(&head, 5);
+        insert_beg(&head, 7);
+        insert_beg(&head, 5);
+        insert_beg(&head, 8);
+        insert_beg(&head, 1);
+        print(head);
+        printf("\n");
+        delete_begin(&head);
+        print(head);
+}
